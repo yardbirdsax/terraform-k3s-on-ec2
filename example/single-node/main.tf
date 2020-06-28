9bcd79b2abc216cc8926735b6e5110ac9bfde4d2
Josh Feierman <>
2020-06-27 21:13:24 -0400

    Corrected issue where outbound traffic was not allowed by security group of master in example setup.

--- example/single-node/main.tf
+++ example/single-node/main.tf
@@ -24,6 +24,15 @@ resource aws_security_group_rule allow_all_inbound {
   security_group_id = aws_security_group.sg.id
 }
 
+resource aws_security_group_rule allow_all_outbound {
+  cidr_blocks = [ "0.0.0.0/0" ]
+  from_port = 0
+  to_port = 65535
+  type = "egress"
+  protocol = "-1"
+  security_group_id = aws_security_group.sg.id
+}
+
 module "ssh_key_pair" {
   source                = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=master"
   namespace             = local.deployment_name
