b85fb152c998e2383d9360041fbf4ef5699310b5
Josh Feierman <josh@sqljosh.com>
2021-11-21 09:57:11 -0500

    Format fixes

--- example/single-node/main.tf
+++ example/single-node/main.tf
@@ -1,4 +1,4 @@
-provider aws {
+provider "aws" {
   region = "us-east-2"
 }
 
@@ -6,30 +6,30 @@ locals {
   deployment_name = "josh-test"
 }
 
-data aws_vpc vpc {
+data "aws_vpc" "vpc" {
   default = true
 }
 
-resource aws_security_group sg {
-  name = "allow_all"
+resource "aws_security_group" "sg" {
+  name   = "allow_all"
   vpc_id = data.aws_vpc.vpc.id
 }
 
-resource aws_security_group_rule allow_all_inbound {
-  cidr_blocks = [ "0.0.0.0/0" ]
-  from_port = 0
-  to_port = 65535
-  type = "ingress"
-  protocol = "-1"
+resource "aws_security_group_rule" "allow_all_inbound" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 65535
+  type              = "ingress"
+  protocol          = "-1"
   security_group_id = aws_security_group.sg.id
 }
 
-resource aws_security_group_rule allow_all_outbound {
-  cidr_blocks = [ "0.0.0.0/0" ]
-  from_port = 0
-  to_port = 65535
-  type = "egress"
-  protocol = "-1"
+resource "aws_security_group_rule" "allow_all_outbound" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 65535
+  type              = "egress"
+  protocol          = "-1"
   security_group_id = aws_security_group.sg.id
 }
 
@@ -45,21 +45,21 @@ module "ssh_key_pair" {
   public_key_extension  = ".pub"
 }
 
-module k3s {
+module "k3s" {
   source = "../../terraform"
   providers = {
     aws = aws
   }
 
-  assign_public_ip = true
-  deployment_name = "jef-test"
-  instance_type = "t3a.small"
-  keypair_content = module.ssh_key_pair.public_key
-  security_group_ids = [ aws_security_group.sg.id ]
-  kubeconfig_mode = "644"
-  
+  assign_public_ip   = true
+  deployment_name    = "jef-test"
+  instance_type      = "t3a.small"
+  keypair_content    = module.ssh_key_pair.public_key
+  security_group_ids = [aws_security_group.sg.id]
+  kubeconfig_mode    = "644"
+
 }
 
-output k3s_master_public_dns {
+output "k3s_master_public_dns" {
   value = module.k3s.instance.public_dns
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,89 +1,89 @@
-variable keypair_path {
-    type = string
-    default = ""
-    description = "The path to the public key to use for the instance."
+variable "keypair_path" {
+  type        = string
+  default     = ""
+  description = "The path to the public key to use for the instance."
 }
 
-variable keypair_content {
-    type = string
-    default = ""
-    description = "The raw data to be used for the public key for the instance. If this is used, no value must be specified for 'keypair_path'."
+variable "keypair_content" {
+  type        = string
+  default     = ""
+  description = "The raw data to be used for the public key for the instance. If this is used, no value must be specified for 'keypair_path'."
 }
 
-variable deployment_name {
-    type = string
-    default = "k3s"
-    description = "A unique name used to generate other names for resources, such as instance names."
+variable "deployment_name" {
+  type        = string
+  default     = "k3s"
+  description = "A unique name used to generate other names for resources, such as instance names."
 }
 
-variable iam_role_name {
-    type = string
-    default = null
-    description = "The name of an IAM Role to assign to the instance. If left blank, no role will be assigned."
+variable "iam_role_name" {
+  type        = string
+  default     = null
+  description = "The name of an IAM Role to assign to the instance. If left blank, no role will be assigned."
 }
 
-variable subnet_id {
-    type = string
-    default = ""
-    description = "The ID of a VPC subnet to assign the instance to. If left blank, the instance will be provisioned in the default subnet of the default VPC."
+variable "subnet_id" {
+  type        = string
+  default     = ""
+  description = "The ID of a VPC subnet to assign the instance to. If left blank, the instance will be provisioned in the default subnet of the default VPC."
 }
 
-variable security_group_ids {
-    type = list(string)
-    description = "A list of Security Group IDs to assign to the instance."
+variable "security_group_ids" {
+  type        = list(string)
+  description = "A list of Security Group IDs to assign to the instance."
 }
 
-variable assign_public_ip {
-    type = bool
-    default = true
-    description = "If set to 'true', a public IP address will be assigned to the instance."
+variable "assign_public_ip" {
+  type        = bool
+  default     = true
+  description = "If set to 'true', a public IP address will be assigned to the instance."
 }
 
-variable instance_type {
-    type = string
-    default = "t3.small"
-    description = "The AWS EC2 Instance Type to provision the instance as."
+variable "instance_type" {
+  type        = string
+  default     = "t3.small"
+  description = "The AWS EC2 Instance Type to provision the instance as."
 }
 
-variable manifest_bucket_path {
-    type = string
-    default = ""
-    description = "The AWS S3 bucket name and path that will be used to download manifest files for auto-installation as per [this documentation](https://rancher.com/docs/k3s/latest/en/advanced/). Should be specified as 'bucket name/folder name/'. The IAM Role assigned to the instance must have GetObject access to this bucket."
+variable "manifest_bucket_path" {
+  type        = string
+  default     = ""
+  description = "The AWS S3 bucket name and path that will be used to download manifest files for auto-installation as per [this documentation](https://rancher.com/docs/k3s/latest/en/advanced/). Should be specified as 'bucket name/folder name/'. The IAM Role assigned to the instance must have GetObject access to this bucket."
 }
 
-variable enable_worker_nodes {
-    type = bool
-    description = "If set to 'true', a separate autoscaling group will be created for worker nodes."
-    default = false
+variable "enable_worker_nodes" {
+  type        = bool
+  description = "If set to 'true', a separate autoscaling group will be created for worker nodes."
+  default     = false
 }
 
-variable worker_node_min_count {
-    type = number
-    description = "The minimum number of worker node instances to provision."
-    default = 0
+variable "worker_node_min_count" {
+  type        = number
+  description = "The minimum number of worker node instances to provision."
+  default     = 0
 }
 
-variable worker_node_max_count {
-    type = number
-    description = "The maximum number of worker node instances to provsion."
-    default = 0
+variable "worker_node_max_count" {
+  type        = number
+  description = "The maximum number of worker node instances to provsion."
+  default     = 0
 }
 
-variable worker_node_desired_count {
-    type = number
-    description = "The desired number of worker nodes to provision." 
-    default = 0
+variable "worker_node_desired_count" {
+  type        = number
+  description = "The desired number of worker nodes to provision."
+  default     = 0
 }
 
 variable "kubeconfig_mode" {
-  type = string
+  type        = string
   description = "Sets the file mode of the generated KUBECONFIG file on the master k3s instance. Defaults to '600'."
-  default = "600"
+  default     = "600"
 }
 
 variable "ami_id" {
-  type = string
+  type        = string
   description = "The AMI ID to use when provisioning the instance. If left at the default null value, the latest Ubuntu server image is used."
-  default = null
+  default     = null
 }
 
